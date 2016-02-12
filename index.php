eab8d81b47b8240ac6356c37cccde4abda26dafd
gregmuldunna <bobsaludo@gmail.com>
2016-02-11 22:09:16 -0800

    Base structure w/o styling nor img

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Bob Saludo DWA P1 Portfolio</title>
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- add a favicon -->
+	</head>
+
+	<body>
+		Bob Saludo - Dynamic Web Applications Course Portfolio
+
+		<button>Password Generator</button>
+		<button>Developer's Best Friend</button>
+		<button>Final Project</button>
+
+		<!-- Add image -->
+	</body>
+
+
+</html>
