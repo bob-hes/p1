7ebf0e5cda4ad7ac36a8c295455d34250cf73696
gregmuldunna <bobsaludo@gmail.com>
2016-02-12 01:01:04 -0800

    Basic outline set up with some styling

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Bob Saludo DWA P1 Portfolio</title>
 		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="shortcut icon" type="image/png" href="images/favicon.ico"/>
+	
+		<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
 	</head>
 
 	<body>
-		Bob Saludo - Dynamic Web Applications Course Portfolio
+	<div class="pure-g content">
 
-		<button>Password Generator</button>
-		<button>Developer's Best Friend</button>
-		<button>Final Project</button>
+		<div class="pure-u-1-3 sidebar">
 
-		<!-- Add image -->
+			<div class="sidebar-intro">
+				
+				<img src="images/selfie.png" />
+				
+				<p>Bob Saludo</p>
+				<p>Dynamic Web Applications Course Portfolio</p>
+
+				<p>Why, hello there. It's seem you've stumbled upon my portfolio. Here you will find links to projects for the Dynamic Web Applications Course (CSCI-E 15) for the 2016 Spring Semester. Don't you worry about the unfinished links. Most of them will be coming soon as the semester goes by.</p>
+			</div>
+		</div>
+
+		<div class="pure-u-2-3 projects">
+			<div class="project">
+				Password Generator
+			</div>
+			<div class="project">
+				Developer's Best Friend
+			</div>
+			<div class="project">
+				Final Project
+			</div>
+		</div>
+
+	</div>
 	</body>
 
 
