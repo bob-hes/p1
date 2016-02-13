c5946e21e1c8540be65a39cf07c7433152283820
gregmuldunna <bobsaludo@gmail.com>
2016-02-13 12:38:16 -0800

    Cleanup on the color and the github links

--- index.php
+++ index.php
@@ -20,21 +20,29 @@
 				
 				<img src="images/selfie.png" />
 				
-				<p>Bob Saludo</p>
+				<h1>Bob Saludo</h1>
 				<p>Dynamic Web Applications Course Portfolio</p>
 
-				<p>Why, hello there. It's seem you've stumbled upon my portfolio. Here you will find links to projects for the Dynamic Web Applications Course (CSCI-E 15) for the 2016 Spring Semester. Don't you worry about the unfinished links. Most of them will be coming soon as the semester goes by.</p>
+				<p class="sidebar-intro-msg">Why, hello there. It's seem you've stumbled upon my portfolio. Here you will find links to projects for the Dynamic Web Applications Course (CSCI-E 15) for the 2016 Spring Semester. Don't you worry about the unfinished links. Most of them will be coming soon as the semester goes by.</p>
 
 				<p>
-				The source code for this site is found here: <a target="_blank" src="https://github.com/bob-hes/p1"><img class="gitlink" /></a>
+				The source code for this site is found here: 
+				<a target="_blank" href="https://github.com/bob-hes/p1">
+					<img src="http://img11.deviantart.net/b290/i/2012/223/4/3/github_flurry_ios_style_icon_by_flakshack-d5ariic.png" id="sidebar-intro-gitlink" />
+				</a>
 				</p>
 			</div>
 		</div>
 
 		<div class="pure-u-2-3 projects">
 			<div class="project">
-				<h1>Password Generator</h1> <a target="_blank" src=""><img class="gitlink" /></a>
-				
+				<h1>
+					Password Generator
+				</h1> 
+
+				<p>
+				In development...
+				</p>
 				<p>
 				Inspired by <a href="https://xkcd.com/936/" target="_blank">this xkcd comic strip</a>, the password generator will spit out a password that will still be hard to break but will be easier to remember. 
 				</p>
@@ -45,8 +53,13 @@
 			</div>
 			<hr>
 			<div class="project">
-				<h1>Developer's Best Friend</h1> <a target="_blank" src=""><img class="gitlink" /></a>
+				<h1>
+					Developer's Best Friend 
+				</h1>
 
+				<p>
+				Coming soon...
+				</p>
 				<p>
 				Getting data to test out applications should no time. In this application, the Lorem Ipsum Generator will create filler text to test out the textual display of a site.
 				</p>
@@ -57,7 +70,8 @@
 			</div>
 			<hr>
 			<div class="project">
-				<h1>Final Project</h1> <a target="_blank" src=""><img class="gitlink" /></a>
+				<h1>Final Project
+				</h1> 
 				Coming soon...
 			</div>
 		</div>
